0b0d7d1f843fac9aa36585d62d845727d8bc9d2e
mans5834 <105821977+mans5834@users.noreply.github.com>
2022-05-26 19:34:27 -0700

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -4,7 +4,7 @@ int main(){
   int UserInput = 0;
   Introduction();
   cin >> UserInput;
-  while(UserInput < 1 && UserInput > 4){
+  while(UserInput < 1 || UserInput > 4){
     cout << "That is not an option please enter another option." << endl;
     cin >> UserInput;
   }
